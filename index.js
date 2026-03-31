77463f0aa9a62499f54f3f907ecec3ee8d781109
harrison-artisan <harrison@artisan.com.au>
2026-03-31 13:55:24 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const cron = require('node-cron');
 const jobAlertsController = require('./controllers/jobAlertsController');
